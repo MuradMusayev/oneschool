984cab229f2cfad2169d77beefe62f88ab234262
ElcinQuluyev <elcinq743@gmail.com>
2024-06-16 17:33:19 +0400

    add:oneschool

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 const listElem = document.querySelectorAll(".menu li a");
+const menuBtn = document.querySelector(".toggle-menu-btn");
+const closeBtn = document.querySelector(".close-btn");
+const menuElem = document.querySelector(".menu");
+const contactBtn = document.querySelector("header a.btn");
 
 listElem.forEach((li) =>
   li.addEventListener("click", () => {
@@ -6,3 +10,13 @@ listElem.forEach((li) =>
     li.classList.add("active");
   })
 );
+
+menuBtn.addEventListener("click", () => {
+  menuElem.style.right = "0px";
+  contactBtn.style.right = "25px";
+});
+
+closeBtn.addEventListener("click", () => {
+  menuElem.style.right = "-185px";
+  contactBtn.style.right = "-185px";
+});
