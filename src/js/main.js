ec44feaa6e8058dfa03d1c12e7b0320031350b60
ElcinQuluyev <elcinq743@gmail.com>
2024-06-17 17:07:36 +0400

    oneschool

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ const menuBtn = document.querySelector(".toggle-menu-btn");
 const closeBtn = document.querySelector(".close-btn");
 const menuElem = document.querySelector(".menu");
 const contactBtn = document.querySelector("header a.btn");
+const headerElem = document.querySelector("header");
+const introElem = document.querySelector("#intro");
 
 listElem.forEach((li) =>
   li.addEventListener("click", () => {
@@ -19,4 +21,18 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menuElem.style.right = "-185px";
   contactBtn.style.right = "-185px";
+});
+
+window.addEventListener("scroll", () => {
+  let topLev = introElem.getBoundingClientRect().top;
+  if (topLev === 0) {
+    headerElem.classList.remove("header-active");
+  } else {
+    headerElem.classList.add("header-active");
+    if (topLev < -80) {
+      headerElem.style.paddingBlock = "0.5rem";
+    } else {
+      headerElem.style.paddingBlock = "1.5rem";
+    }
+  }
 });
